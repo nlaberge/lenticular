e06f276ebdc2584149c8766dfefbb368904297dc
nlaberge <43562244+nlaberge@users.noreply.github.com>
2022-12-25 11:54:15 -0800

    writing documentation

--- code/lentnickular.py
+++ code/lentnickular.py
@@ -1,14 +1,6 @@
 # import image libraries
 from PIL import Image
 
-# Image parameters, only change the variables in all caps
-IMAGE_WIDTH_INCHES = 6
-IMAGE_HEIGHT_INCHES = 4
-IMAGE_DPI = 600
-image_width_px = IMAGE_WIDTH_INCHES * IMAGE_DPI
-CPI = 49.66
-num_colums = CPI*IMAGE_WIDTH_INCHES
-px_per_column = image_width_px / num_colums
 
 def weave_images_together(im1_path,im2_path,px_per_column):
     # load two images from jpeg files
@@ -27,10 +19,34 @@ def weave_images_together(im1_path,im2_path,px_per_column):
 # take command line arguments in main function
 if __name__ == '__main__':
     import argparse
+    # Image parameters, only change the variables in all caps
+    IMAGE_WIDTH_INCHES = 6
+    IMAGE_HEIGHT_INCHES = 4
+    IMAGE_DPI = 600
+    CPI = 49.66
+
+    # take command line arguments for image paths
     parser = argparse.ArgumentParser(description='Weave two images together')
     parser.add_argument('image1', help='Path to first image')
     parser.add_argument('image2', help='Path to second image')
+
+    #add optional arguments
+    parser.add_argument('-cpi', help='CPI of the lenticular sheet', type=float, default=CPI)
+    parser.add_argument('-dpi', help='DPI of the output image', type=int, default=IMAGE_DPI)
+    parser.add_argument('-width', help='Width of the output image in inches', type=float, default=IMAGE_WIDTH_INCHES)
+    parser.add_argument('-height', help='Height of the output image in inches', type=float, default=IMAGE_HEIGHT_INCHES)
+
     args = parser.parse_args()
+    # update variables from command line arguments
+    IMAGE_WIDTH_INCHES = args.width
+    IMAGE_HEIGHT_INCHES = args.height
+    IMAGE_DPI = args.dpi
+    CPI = args.cpi
+
+    image_width_px = IMAGE_WIDTH_INCHES * IMAGE_DPI
+    num_colums = CPI*IMAGE_WIDTH_INCHES
+    px_per_column = image_width_px / num_colums
+
     im_weave = weave_images_together(args.image1,args.image2,px_per_column)
     im_weave.save('lenticular_output.png',dpi=(IMAGE_DPI,IMAGE_DPI))
     print('saved image to lenticular_output.png')
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Making Images for Lenticular Lenses by Nick
+
+
+Run lentnickular.py and provide the paths to the images that you want to merge. Example:
+
+ ``code/lentnickular.py figures/yawn_a.JPG figures/yawn_b.JPG -dpi 600 -cpi 50``
+
+Required arguments:
+- First image path
+- Second image path
+
+Optional arguments: 
+- -dpi (default = 600)
+- -cpi (columns per inch, default = 49.66)
+- -width (output image width in inches, default = 6)
+- -height (output image height in inches, default = 4)
